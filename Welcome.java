54d789cc7f821dd16fe205f4b60865b9d44b824a
Skater_Jibin <141441054+jibin03@users.noreply.github.com>
2024-03-02 22:25:22 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Welcome
+{
+    public static void main(String args[])
+    {
+        System.out.println("Jibin Sabu John");
+        System.out.println("Welcome to java");
+    }
+}
